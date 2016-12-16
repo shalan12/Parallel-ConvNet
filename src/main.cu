2ad229231ad84bd55a5a0fa316b54ad2872ccc25
Syed Shalan Naqvi <naqvi5@siebl-0403-24.ews.illinois.edu>
2016-12-16 03:04:45 -0600

    added parallel Relu4

--- src/main.cu
+++ src/main.cu
@@ -17,8 +17,11 @@
 #define NUM_CHANNELS 1
 #define NUM_DIGITS 10
 #define TILE_SIZE 16
+
 #define POOL_TILE_SIZE 4
 #define POOL_BLOCK_OUTPUT_FEATURE 32 //so all accesses in a warp are consecutive
+
+#define RELU_TILE_SIZE 10
 
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
@@ -52,6 +55,8 @@ void parallel_pool_wrapper(const float *X, const int xdims[4],
                          const int pool_size, float *Y, const int ydims[4]);
 __global__ void parallel_pool(const float *X, const int xdims[4],
                          const int pool_size, float *Y, const int ydims[4], int wGrid);
+void parallelRelu4Wrapper(float *X, const int xdims[4]);
+__global__ void parallelRelu4(float *X, const int size);
 
 static int loadData(float *x, float *y) {
   // Open the data file
@@ -278,6 +283,30 @@ static void relu4(float *X, const int xdims[4]) {
   }
 }
 
+void parallelRelu4Wrapper(float *X, const int xdims[4]) {
+  const int numElemsX  = multiplyArr(xdims, 4);
+  int sizeX = numElemsX * sizeof(float);
+  float * deviceX;
+  wbCheck(cudaMalloc(&deviceX, sizeX));
+  wbCheck(cudaMemcpy(deviceX, X, sizeX, cudaMemcpyHostToDevice));
+  const int numThreads = 512;
+  const int numBlocks = numElemsX/(numThreads*RELU_TILE_SIZE);
+  parallelRelu4<<<numBlocks, numThreads>>>(deviceX, numElemsX);
+  wbCheck(cudaMemcpy(X, deviceX, sizeX, cudaMemcpyDeviceToHost));
+}
+
+__global__ void parallelRelu4(float *X, const int size) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = 0; i < RELU_TILE_SIZE; i++) {
+    if (idx > size) break;
+    else {
+      X[idx] = (X[idx] < 0) ? 0 : X[idx];
+      idx += stride;
+    }
+  } 
+}
+
 // Recified linear unit 2d
 static void relu2(float *X, const int xdims[2]) {
   for (const auto i : range(0, xdims[0] * xdims[1])) {
@@ -415,7 +444,9 @@ void forward_operation(float *x, float *conv1, float *conv2, float *fc1,
   //conv_forward_valid(x, xdims, conv1, conv1dims, a, adims);
   
   /// relu layer
-  relu4(a, adims);
+  //relu4(a, adims);
+  parallelRelu4Wrapper(a, adims);
+
 
   // average pooling
   const int pool_size = 2;
@@ -432,8 +463,9 @@ void forward_operation(float *x, float *conv1, float *conv2, float *fc1,
   easyConvWrapper(b, bdims, conv2, conv2dims, c, cdims);
   //conv_forward_valid(b, bdims, conv2, conv2dims, c, cdims);
   // relu
-  relu4(c, cdims);
-
+  //relu4(c, cdims);
+  parallelRelu4Wrapper(c, cdims);
+  
   // average pooling
   const int ddims[] = {cdims[0], cdims[1] / pool_size, cdims[2] / pool_size,
                        cdims[3]};
@@ -454,6 +486,7 @@ void forward_operation(float *x, float *conv1, float *conv2, float *fc1,
 
   // matrix multiplication
   const int fdims[] = {edims[0], fc2dims[1]};
+
   auto f            = zeros<float>(fdims);
   fully_forward(e, edims, fc2, fc2dims, f, fdims);
 
